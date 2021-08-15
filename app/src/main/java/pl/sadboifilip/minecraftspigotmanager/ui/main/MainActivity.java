eb4fcacb27574ffc39c0fb7cde908602f009a9e4
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-08-15 02:24:18 +0200

    Added opening other activity

--- app/src/main/java/pl/sadboifilip/minecraftspigotmanager/ui/main/MainActivity.java
+++ app/src/main/java/pl/sadboifilip/minecraftspigotmanager/ui/main/MainActivity.java
@@ -1,9 +1,11 @@
 package pl.sadboifilip.minecraftspigotmanager.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import pl.sadboifilip.minecraftspigotmanager.databinding.ActivityMainBinding;
+import pl.sadboifilip.minecraftspigotmanager.ui.players.PlayersActivity;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -14,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
         this.binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
+
+        this.binding.buttonPlayersActivity.setOnClickListener(e->{
+            Intent intent = new Intent(this, PlayersActivity.class);
+            startActivity(intent);
+        });
     }
 }
