2acd01f41046ed311a59cf8b00d8e7eebf097cc5
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-08-20 01:01:23 +0200

    Added saving server creds

--- app/src/main/java/pl/sadboifilip/minecraftspigotmanager/ui/main/MainActivity.java
+++ app/src/main/java/pl/sadboifilip/minecraftspigotmanager/ui/main/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import pl.sadboifilip.minecraftspigotmanager.databinding.ActivityMainBinding;
@@ -12,6 +14,7 @@ import pl.sadboifilip.minecraftspigotmanager.ui.players.PlayersActivity;
 public class MainActivity extends AppCompatActivity {
     private static String SERVER_URL = "server_url";
     private static String SERVER_TOKEN = "server_token";
+    private static String SERVER_INFO_SAVE_MESSAGE = "Server creds successfully saved!";
 
     private ActivityMainBinding binding;
 
@@ -27,9 +30,19 @@ public class MainActivity extends AppCompatActivity {
         this.binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
 
-        this.binding.buttonPlayersActivity.setOnClickListener(e->{
+        this.binding.activityMainButtonPlayersActivity.setOnClickListener(e->{
             Intent intent = new Intent(this, PlayersActivity.class);
             startActivity(intent);
+        });
+        this.binding.activityMainButtonServerInfoSave.setOnClickListener(e->{
+            final SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(SERVER_URL,this.binding.activityMainUrl.getText().toString());
+            editor.putString(SERVER_TOKEN,this.binding.activityMainToken.getText().toString());
+            editor.apply();
+
+            Toast toast = Toast.makeText(getApplicationContext(), SERVER_INFO_SAVE_MESSAGE, Toast.LENGTH_SHORT);
+            toast.show();
+
         });
 
         this.binding.activityMainUrl.setText(url);
